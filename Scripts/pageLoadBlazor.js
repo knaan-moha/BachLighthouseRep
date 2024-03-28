b59025ccf1223e72d1ccf90bbb967feb8e207f9f
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-03-28 03:22:26 +0100

    :sparkles: Feat: add pageload for blazor with diff browsers

---
+++ Scripts/pageLoadBlazor.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer"); // v20.7.4 or later
+
+const browserPaths = {
+  //edge: "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge",
+  //chrome: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  brave: "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
+};
+
+// make the browser
+const browserType = "brave";
+
+const performTasksAndGenerateReport = async (trails_num) => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    // use the default size of the browser window
+    defaultViewport: null,
+    executablePath: browserPaths[browserType],
+  });
+  const page = await browser.newPage();
+  const timeout = 5000;
+  page.setDefaultTimeout(timeout);
+
+  const lhApi = await import("lighthouse"); // v10.0.0 or later
+  const flags = {
+    emulatedFormFactor: "desktop",
+    throttlingMethod: "simulate",
+  };
+  const config = lhApi.desktopConfig;
+  const lhFlow = await lhApi.startFlow(page, {
+    name: "reactpageLoads",
+    config,
+    flags,
+  });
+
+  await lhFlow.startNavigation();
+  {
+    const targetPage = page;
+    const promises = [];
+    const startWaitingForEvents = () => {
+      promises.push(targetPage.waitForNavigation());
+    };
+    startWaitingForEvents();
+    await targetPage.goto("http://localhost:5092/");
+    await Promise.all(promises);
+  }
+
+  await page.deleteCookie(...(await page.cookies()));
+
+  //? Clear localStorage and sessionStorage after the page has loaded
+  try {
+    await page.evaluate(() => {
+      localStorage.clear();
+      sessionStorage.clear();
+    });
+  } catch (err) {
+    console.error("Error clearing storage:", err);
+  }
+  await lhFlow.endNavigation();
+  const lhFlowReport = await lhFlow.generateReport();
+  let reportPath = `../Pageloads/notDeployed/Blazor/${browserType}`;
+  fs.mkdirSync(reportPath, { recursive: true });
+
+  const reportFilename = `blazorPageLoad${trails_num}ReportLight.html`;
+  try {
+    fs.writeFileSync(path.join(reportPath, reportFilename), lhFlowReport);
+    //console.log("Report saved successfully!");
+  } catch (error) {
+    console.error("Error saving the report:", error);
+  }
+  await browser.close();
+};
+(async () => {
+  for (let trialNumber = 1; trialNumber <= 2; trialNumber++) {
+    console.log(`Starting trial ${trialNumber}...`);
+    await performTasksAndGenerateReport(trialNumber).catch((err) => {
+      console.error(`Error in trial ${trialNumber}:`, err);
+    });
+  }
+})();
